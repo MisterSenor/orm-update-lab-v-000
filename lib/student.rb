ef6961e30508f460856458a6dfcc904b98a9107b
Nathan  Visak <nathan.visak@gmail.com>
2018-07-23 20:36:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name)
     binding.pry
   end
 
