dc83d8ecb27b3a9776eb21d3db72524c7f0c2cc2
Nathan  Visak <nathan.visak@gmail.com>
2018-07-23 20:18:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,5 +53,18 @@ class Student
     student
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    #where is the row argument coming from?  Another method? 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end 
+  
+    
+    
+    
+
 
 end
